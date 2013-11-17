55244f13924031092c8dbf1379dae1db2b860696
root <root@web-01.enet.local>
2013-11-17 11:08:26 -0500

    External file reading and parsing is done!
    Note that this is very dirty. I think the input document has about three to four copies of itself in memory. One to read it in, one with modified contents, one modified into XML elements, and one that might be made when associating the element with the page document. Really, we should be reading in one line at a time, parsing that line, and injecting it directly into the page object.

--- index.php
+++ index.php
@@ -1,23 +1,53 @@
 <?php
 
+// Notes:
+// This is so dangerous if the doc doesn't fit into memory...
+// I need to find a safer way to parse/inject the contents into the XML.
+
+require_once("wiky.inc.php");
+
 $THEME = "default";
 $THEME_LOCATION = "themes/" . $THEME;
 
-$doc = createPage("TLW", "Hello world", $THEME_LOCATION);
+$wikiFormatter = new wiky();
+
+$wikiDoc = parseWikiDoc("input.wiki");
+$doc = createPage("TLW", $wikiDoc, "2011-01-01"); 
 
 header("Content-type: text/xml; charset=utf-8");
 $doc->save("php://output");
 
-function createPage($title, $body, $themeLocation) {
+function parseWikiDoc($file)
+{
+	global $wikiFormatter;
+
+	$wikiText = file_get_contents($file);
+	$wikiText = htmlspecialchars($wikiText);
+	return $wikiFormatter->parse($wikiText);
+}
+
+function createPage($title, $body, $date, $themeLocation = null) {
 	$doc = new DOMDocument('1.0', 'UTF-8');
+
+	global $THEME_LOCATION;
+	if ($themeLocation == null)
+		$themeLocation = $THEME_LOCATION;
+
+	if ($body == "")
+		$body = "No body text given to createPage on line " . __LINE__;
+
+	// Turn body HTML into a real document
+	$bodyContentNode = DOMDocument::loadXML("<body>" . $body . "</body>");
+	$bodyContentNode = $bodyContentNode->getElementsByTagName("body")->item(0);
+	$bodyContentNode = $doc->importNode($bodyContentNode, true);
 
 	// Create nodes
 	$xlsNode     = new DOMProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="' . $themeLocation . '/main.xsl"');
 	$docNode     = new DOMElement("document");
 	$titleNode   = new DOMElement("title", $title); 
-	$bodyNode    = new DOMElement("body", $body);
+	$bodyNode    = new DOMElement("body");
 	$metaNode    = new DOMElement("meta");
-	$dateNode    = new DOMElement("date", "2013-11-16");
+	$dateNode    = new DOMElement("date", $date);
 	$authNode    = new DOMElement("author", "Edwin Amsler");
 	$styleNode   = new DOMElement("style");
 	$fileNode    = new DOMElement("file", $themeLocation . "/main.css");
@@ -27,6 +57,7 @@ function createPage($title, $body, $themeLocation) {
 	$doc->appendChild($docNode);
 	$docNode->appendChild($titleNode);
 	$docNode->appendChild($bodyNode);
+	$bodyNode->appendChild($bodyContentNode);
 	$docNode->appendChild($metaNode);
 	$metaNode->appendChild($dateNode);
 	$metaNode->appendChild($authNode);
